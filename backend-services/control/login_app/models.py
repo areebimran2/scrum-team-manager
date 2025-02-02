7b9ef53b605fb80ca81c99c61709d245d65002dd
TEDiks2003 <maciej.kwasnik@gmail.com>
2025-02-02 00:10:27 -0500

    SCRUM-23: Started Login App

---
+++ backend-services/control/login_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class UserModel(models.Model):
+    #TODO Max Length to be confirmed
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 50)
