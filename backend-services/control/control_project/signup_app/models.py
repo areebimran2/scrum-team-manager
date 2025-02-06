0bf5482225617dd92a0f2b05c6cd5058846b9c8c
Maciej Kwasnik <117639273+TEDiks2003@users.noreply.github.com>
2025-02-06 18:52:45 -0500

    SCRUM-23 fixed user models (#22)

--- backend-services/control/control_project/signup_app/models.py
+++ backend-services/control/control_project/signup_app/models.py
@@ -3,10 +3,5 @@ from django.db import models
 # Create your models here.
 
 class UserSignUpModel(models.Model):
-    #TODO Max Length to be confirmed
-<<<<<<< HEAD
     email = models.EmailField()
-=======
-    email = models.EmailFieldField()
->>>>>>> origin/feature/SCRUM-5-user-login
     password = models.CharField(max_length=100)
