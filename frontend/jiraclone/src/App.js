76740e16c906473fa18ffbecd2a99ea05ba5dd4b
wangs678 <simina.wang@mail.utoronto.ca>
2025-02-06 17:52:57 -0500

    SCRUM-31 Added the password recovery request and success pages to react with a css module file for UI, also added the import statements into App.js

--- frontend/jiraclone/src/App.js
+++ frontend/jiraclone/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import NotFound from "./pages/404";
+import RecoveryRequest from "./pages/recoveryrequest";
+import RecoveryRequestSuccess from "./pages/recoveryrequestsuccess";
 
 export function App() {
   return (
@@ -9,6 +11,8 @@ export function App() {
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/recoveryrequest" element={<RecoveryRequest />} />
+            <Route path="/recoveryrequestsuccess" element={<RecoveryRequestSuccess />} />
             <Route path="/" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
