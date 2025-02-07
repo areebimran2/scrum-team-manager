4f6ba5cbe1fb144a310bf44b7611b4fb5ddbe422
Leon Nitsch <leon.nitsch@mail.utoronto.ca>
2025-02-06 21:54:57 -0500

    SCRUM-22: Created dashboard page and began designing for css

--- frontend/jiraclone/src/pages/dashboard.js
+++ frontend/jiraclone/src/pages/dashboard.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useNavigate } from 'react-router-dom';
+import styles from '../styles/dashboard.module.css';
+
+
+
+
 export function Dashboard() {
   return (
-    <div>
-      <h1>Welcome to the Dashboard</h1>
-      <p>This is your project management dashboard.</p>
-    </div>
+    <body className={styles.body}>
+      <div className={styles.titleContainer}>
+
+        <h1 className={styles.title}>Dashboard</h1>
+
+      </div>
+
+      
+    </body>
   );
 }
 
