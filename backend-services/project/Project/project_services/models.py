dad27867406b8bc47e69a69a22f35c67f1323703
Assad Usman <assadusman@assads-air.home>
2025-02-19 15:55:28 -0500

    created model and get/create/update/delete endpoints

---
+++ backend-services/project/Project/project_services/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    pid = models.IntegerField("Project Id", primary_key=True)
+    name = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=500, default="")
+    tickets = models.JSONField(default=list)
+    creator = models.IntegerField()
+    date_created = models.CharField(max_length=100)
+    scrum_users = models.JSONField(default=list)
+    # changed this to a list
+    admin = models.JSONField(default=list)
