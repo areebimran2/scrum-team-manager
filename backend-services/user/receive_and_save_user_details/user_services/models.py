702447b6d6df2043f6a0bba3da831cb3e5ab1c98
Assad Usman <assadusman@assads-air.home>
2025-02-27 04:08:30 -0500

    SCRUM-120: Removed num_tickets and project field in model

--- backend-services/user/receive_and_save_user_details/user_services/models.py
+++ backend-services/user/receive_and_save_user_details/user_services/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class ScrumUser(models.Model):
     uid = models.IntegerField("User Id", primary_key=True)
     assigned_tickets =  models.JSONField(default=list)
-    project = models.JSONField(default=list)
-    num_tickets = models.IntegerField(default=0)
+ 
+
 
     email = models.EmailField()
     password = models.CharField(max_length=100)
