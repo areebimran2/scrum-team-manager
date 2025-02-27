3c8fbb45fc81170e8df4cd91e055f3b18347d5db
Assad Usman <assadusman@assads-air.home>
2025-02-27 09:39:50 -0500

    added a page that displays tickets in a grid like manner

--- frontend/jiraclone/src/App.js
+++ frontend/jiraclone/src/App.js
@@ -6,21 +6,25 @@ import RecoveryRequestSuccess from "./pages/recoveryrequestsuccess";
 import Signup from "./pages/signup";
 import ProfileEdit from "./pages/profileedit"
 import Dashboard from "./pages/dashboard";
+import ProjectList from "./pages/projectlist";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css'
 
 export function App() {
 
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile" element={<ProfileEdit />} />
-            <Route path="/recoveryrequest" element={<RecoveryRequest />} />
-            <Route path="/recoveryrequestsuccess" element={<RecoveryRequestSuccess />} />
-            <Route path="/" element={<NotFound />} />
-        </Routes>
-    </BrowserRouter>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/profile" element={<ProfileEdit />} />
+                <Route path="/recoveryrequest" element={<RecoveryRequest />} />
+                <Route path="/recoveryrequestsuccess" element={<RecoveryRequestSuccess />} />
+                <Route path="/" element={<NotFound />} />
+                <Route path="/projectlist" element={<ProjectList />}></Route>
+            </Routes>
+        </BrowserRouter >
     );
 }
 
