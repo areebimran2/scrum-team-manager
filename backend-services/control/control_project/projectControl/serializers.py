b94275b4c78ee5098324cff98c35617b12738e49
TEDiks2003 <maciej.kwasnik@gmail.com>
2025-02-28 01:25:59 -0500

    BUGFIX fixed projectControl configuration and serializer defaults on required fields

--- backend-services/control/control_project/projectControl/serializers.py
+++ backend-services/control/control_project/projectControl/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 from .models import *
 
 class ProjectAddShellSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, allow_null=False, default=None)
-    description = serializers.CharField(required=True, allow_null=False, default=None)
-    creator = serializers.IntegerField(required=True, allow_null=False, default=None)
+    name = serializers.CharField(required=True, allow_null=False)
+    description = serializers.CharField(required=True, allow_null=False)
+    creator = serializers.IntegerField(required=True, allow_null=False)
 
 
 
 class ProjectUpdateShellSerializer(serializers.Serializer):
-    pid = serializers.IntegerField(required=True, allow_null=False, default=None)
+    pid = serializers.IntegerField(required=True, allow_null=False)
     name = serializers.CharField(required=False, allow_null=True, default=None)
     description = serializers.CharField(required=False, allow_null=True, default=None)
 
