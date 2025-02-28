4ad244d88189df6261cc077468035fa43a78cd44
Assad Usman <assadusman@assads-air.home>
2025-02-27 23:26:46 -0500

    SCRUM-52: removed unneeded fields from project add serializer

--- backend-services/control/control_project/projectControl/serializers.py
+++ backend-services/control/control_project/projectControl/serializers.py
@@ -4,11 +4,8 @@ from .models import *
 class ProjectAddShellSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, allow_null=False, default=None)
     description = serializers.CharField(required=True, allow_null=False, default=None)
-    tickets = serializers.JSONField(required=False, allow_null=True, default=None)
     creator = serializers.IntegerField(required=True, allow_null=False, default=None)
-    date_created = serializers.DateTimeField(required=False, allow_null=True, default=None)
-    scrum_users = serializers.JSONField(required=False, allow_null=True, default=None)
-    admin = serializers.JSONField(required=False, allow_null=True, default=None)
+
 
 
 class ProjectUpdateShellSerializer(serializers.Serializer):
