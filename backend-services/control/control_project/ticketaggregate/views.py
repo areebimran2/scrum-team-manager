0a0c22cdff06f2fba43ac639db8c981ed924ae4e
Assad Usman <assadusman@assads-air.home>
2025-03-27 13:55:49 -0400

    task/SCRUM-140 Added auto assign button and did some bug fixing

--- backend-services/control/control_project/ticketaggregate/views.py
+++ backend-services/control/control_project/ticketaggregate/views.py
@@ -10,6 +10,8 @@ import json
 # Create your views here.
 @api_view(['GET'])
 def getTickets(request, pid_str):
+
+
     if request.method == 'GET':
         print("is GET request")
 
@@ -21,48 +23,49 @@ def getTickets(request, pid_str):
         url = "http://127.0.0.1:8001"
 
         response = requests.get(url + f'/project/query/{pid}')
-        
+
+
+# http://127.0.0.1:10001/
         if response.status_code == 404: # Project does not exist
             print("actually 404")
             # Send 404 back to frontend
             return Response(status=status.HTTP_404_NOT_FOUND)
-                
+        
         elif response.status_code == 200: # Project exists
             uid_dict = dict()
             
             project_dict = response.json()
+            print(project_dict)
+            print(project_dict[0].get("scrum_users"))
 
-            for ticketID in project_dict.get("tickets"):
-                try:
-                    ticketResponse = requests.get(url + f'/ticket/query/{ticketID}').json()
-                    if (not uid_dict.get(ticketResponse["assigned"])):
-                        if (ticketResponse["completed"]):
-                            uid_dict[ticketResponse["assigned"]] = [ticketResponse["story_points"], 0]
-                        else:
-                            uid_dict[ticketResponse["assigned"]] = [0, ticketResponse["story_points"] ]
+            for uid in project_dict[0].get("scrum_users"):
+                uid_dict[uid] = [0,0]
+                userResponse = requests.get('http://127.0.0.1:8000' +  f'/user/query/UID/{uid}').json()[0]
+                print("USER, ",userResponse)
+                if (not userResponse.get("assigned_tickets").get(pid_str)):
+                    continue
+                for ticket in userResponse.get("assigned_tickets").get(pid_str):
+                    ticketResponse = requests.get(url +  f'/ticket/query/{ticket}').json()[0]
+                    if ticketResponse["completed"]:
+                        uid_dict[uid][0] += ticketResponse["story_points"]
                     else:
-                        if (ticketResponse["completed"]):
-                            uid_dict[ticketResponse["assigned"]][0] += ticketResponse["story_points"]
-                        else:
-                            uid_dict[ticketResponse["assigned"]][1] += ticketResponse["story_points"]
-                            
-                except:
-                    return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            
+                        uid_dict[uid][1] += ticketResponse["story_points"]
+
             final_json = []
             for uid in uid_dict.keys():
                 try:
-                    userResponse = requests.get(url + f'/user/query/UID/{uid}').json()
+                    userResponse = requests.get('http://127.0.0.1:8000' + f'/user/query/UID/{uid}').json()[0]
                     one_user_dict = dict()
                     one_user_dict["stats"] = uid_dict[uid]
                     # one_user_dict["UID"] = uid
                     one_user_dict["name"] = uid
                     one_user_dict["skills"] = userResponse["skills"]
                     final_json.append(one_user_dict)
-                except:
-                    return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                except Exception as e:
+                    print(e)
+                    return Response(status=status.HTTP_200_OK)
             
-            return Response(json.dumps(final_json), status=status.HTTP_200_OK)
+            return Response(json.dumps(final_json), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
         else:
             return Response(status=response.status_code)
@@ -71,4 +74,35 @@ def getTickets(request, pid_str):
     
 
         
+
+
+
+
+
     
+
+                # for ticketID in project_dict[0].get("tickets"):
+            #     try:
+            #         print(ticketID)
+            #         ticketResponse = requests.get(url +  f'/ticket/query/{ticketID}')
+                    
+            #         ticketResponse = ticketResponse.json()[0]
+            #         print(ticketResponse)
+            #         if (not uid_dict.get(ticketResponse["assigned_to"])):
+            #             if (ticketResponse["completed"]):
+            #                 uid_dict[ticketResponse["assigned_to"]] = [ticketResponse["story_points"], 0]
+            #             else:
+            #                 uid_dict[ticketResponse["assigned_to"]] = [0, ticketResponse["story_points"] ]
+
+            #         else:
+            #             if (ticketResponse["completed"]):
+            #                 uid_dict[ticketResponse["assigned_to"]][0] += ticketResponse["story_points"]
+            #             else:
+            #                 uid_dict[ticketResponse["assigned_to"]][1] += ticketResponse["story_points"]
+                            
+            #     except Exception as err:
+            #         print(err)
+            #         print("ID", ticketID)
+            #         return Response(status=status.HTTP_200_OK)
+            # print(uid_dict)
+
