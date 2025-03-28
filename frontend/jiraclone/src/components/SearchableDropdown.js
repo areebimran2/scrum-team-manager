e2498aac90a78395f1d5a4549f8fe38ff1112db4
wangs678 <simina.wang@mail.utoronto.ca>
2025-03-27 23:16:30 -0400

    SCRUM 135: created an edit skills page

---
+++ frontend/jiraclone/src/components/SearchableDropdown.js
@@ -0,0 +1,84 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./styles/dropdown.module.css";
+
+const SearchableDropdown = ({
+  options,
+  label,
+  id,
+  selectedVal,
+  handleChange
+}) => {
+  const [query, setQuery] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", toggle);
+    return () => document.removeEventListener("click", toggle);
+  }, []);
+
+  const selectOption = (option) => {
+    setQuery(() => "");
+    handleChange(option[label]);
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  function toggle(e) {
+    setIsOpen(e && e.target === inputRef.current);
+  }
+
+  const getDisplayValue = () => {
+    if (query) return query;
+    if (selectedVal) return selectedVal;
+
+    return "";
+  };
+
+  const filter = (options) => {
+    return options.filter(
+      (option) => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1
+    );
+  };
+
+  return (
+    <div className={styles.dropdown}>
+      <div className={styles.control}>
+        <div className={styles.selectedValue}>
+          <input
+            placeholder="Add Skill"
+            ref={inputRef}
+            type="text"
+            value={getDisplayValue()}
+            name="searchTerm"
+            onChange={(e) => {
+              setQuery(e.target.value);
+              handleChange(null);
+            }}
+            onClick={toggle}
+            className={styles.input}
+          />
+        </div>
+        <div className={`${styles.arrow} ${isOpen ? styles.open : ""}`}></div>
+      </div>
+
+      <div className={`${styles.options} ${isOpen ? styles.open : ""}`}>
+        {filter(options).map((option, index) => {
+          return (
+            <div
+              onClick={() => selectOption(option)}
+              className={`${styles.option} ${
+                option[label] === selectedVal ? styles.selected : ""
+              }`}
+              key={`${id}-${index}`}
+            >
+              {option[label]}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SearchableDropdown;
