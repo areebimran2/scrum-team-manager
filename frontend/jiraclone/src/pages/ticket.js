ba33340454fb75aeb004d52b82568303ae4bbb3c
Leon Nitsch <leonnitsch170@gmail.com>
2025-04-01 02:49:18 -0400

    BUGFIX-fixed prerender bug in full ticket view

--- frontend/jiraclone/src/pages/ticket.js
+++ frontend/jiraclone/src/pages/ticket.js
@@ -9,9 +9,9 @@ export function FullTicket() {
     const [searchParams, setSearchParams] = useSearchParams();
     const tid = searchParams.get("tid");
     const [ticket, setTicket] = useState({
-        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        story_points: 20,
+        title: "",
+        description: "",
+        story_points: 16,
         priority: 2
     });
     const [isAdmin, setAdmin] = useState(true);
